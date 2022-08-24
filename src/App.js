b1bc1baabf6ae18bad017ec34e6de879ac27c278
Andrey <Hitmanki@yandex.ru>
2022-08-24 15:51:05 +0400

    Add background, LeftMenu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { LeftMenu } from './Components/LeftMenu';
+import { MainContainer } from './Components/MainContainer';
+import { RightMenu } from './Components/RightMenu';
 
 function App() {
-	return ( <div className="App">
+	return ( 
+		<div className="App">
+			<LeftMenu />
+			<MainContainer />
+			<RightMenu />
 
-	</div>
+			<div className="background"></div>
+		</div>
 	);
 }
 
